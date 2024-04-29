816f283cb8754017d57699ea37a7f27f883b03a9
Marc Cruz <84113211+bakedmarc@users.noreply.github.com>
2024-04-29 16:20:13 +0800

    Flood Code Change
    40% done

--- script.js
+++ script.js
@@ -1,22 +1,18 @@
 let sections = document.querySelectorAll('section');
-let sections = document.querySelectorAll('header nav ul li a');
+let navLinks = document.querySelectorAll('header nav ul li a');
 
 window.onscroll = () => {
-	sections.forEach(sec => {
-		let top = window.scrollY;
-		let offset = sec.offsetTop - 150;
-		let height = sec.offsetHeight;
-		let id = sec.getAttribute('id');
+    sections.forEach(sec => {
+        let top = window.scrollY;
+        let offset = sec.offsetTop - 150;
+        let height = sec.offsetHeight;
+        let id = sec.getAttribute('id');
 
-		if(top >= offset && top < offset + height){
-			navLinkls.forEach(links => {
-				links.classList.remove('active');
-				document.querySelector('header nav ul li a')
-				[href*=' + id + ']').classList.add
-				('active');
-			});
-
-		};
-
-	});
+        if (top >= offset && top < offset + height) {
+            navLinks.forEach(link => {
+                link.classList.remove('active');
+            });
+            document.querySelector('header nav ul li a[href*="' + id + '"]').classList.add('active');
+        }
+    });
 };
